ae605f914c75c33707590206ca59af31ec0f0c6b
Nate McGregor <natecmcgregor@gmail.com>
2017-12-20 14:28:15 -0800

    add seed methods for users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_users
+    seed.generate_posts
+  end
+
+  def generate_users
+    30.times do |i|
+      user = User.create!(name: Faker::LordOfTheRings.character,
+                          email: Faker::Internet.email,
+                          password: Faker::Internet.password)
+    end
+  end
+
+  def generate_posts
+    30.times do |i|
+      post = Post.create!(title: Faker::Book.title,
+                          content: Faker::Hipster.sentence(5),
+                          thumbnail: Faker::Fillmurray.image)
+      post.save!
+    end
+  end
+
+end
+
+p "Created #{Post.count} posts"
+
+Seed.begin
