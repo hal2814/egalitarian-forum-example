be08a5930338238eafd4a4539bab77b932aa0a0a
Nate McGregor <natecmcgregor@gmail.com>
2017-12-20 13:26:06 -0800

    create a user view and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:notice] = "You have successfully signed up!"
+      redirect_to '/'
+    else
+      flash[:alert] = "There was a problem signing up."
+      redirect_to '/sign_up'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
