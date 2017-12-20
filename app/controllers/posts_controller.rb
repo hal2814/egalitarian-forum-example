e9a858338cf7a0a2fb5a6655fb31896f9309e18f
Nate McGregor <natecmcgregor@gmail.com>
2017-12-20 13:15:23 -0800

    add create method to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def create
+    @post = Product.new(post_params)
+    if @post.save
+      flash[:notice] = "Post Created!"
+      redirect_to '/'
+    else
+      render :new
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content,)
+    end
 end
--- db/migrate/20171220003622_create_comments.rb
+++ db/migrate/20171220003622_create_comments.rb
@@ -2,7 +2,6 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.string :content
-      t.float :score
       t.integer :post_id
       t.integer :user_id
 
