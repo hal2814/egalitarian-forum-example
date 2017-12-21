a9242ba10b4a3f00c22afc308d126930639b0dff
Nate McGregor <natecmcgregor@gmail.com>
2017-12-21 10:53:41 -0800

    add relational routes in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # belongs_to :forum, through: :standings
   # has_many :forums, through: :standings
+  has_many :posts
+  has_many :comments
 
   validates_presence_of :name
   validates_presence_of :email
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root to: "posts#index"
 
+  get '/sign_up' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/sign_in' => 'sessions#new'
+  post '/sign_in' => 'sessions#create'
+  get 'sign_out' => 'sessions#destroy'
+
+
   resources :posts do
     resources :comments
   end
