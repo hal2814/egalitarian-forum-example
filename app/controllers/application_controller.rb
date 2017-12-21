465ddb5f72cbdee11552e330912efe7f75eebefd
Nate McGregor <natecmcgregor@gmail.com>
2017-12-20 16:31:46 -0800

    update posts index view page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_users
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize
+    redirect_to '/sign_in' unless admin
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You have successfully signed in!"
+      redirect_to '/'
+    else
+      flash[:alert] = "There was a problem signing in."
+      redirect_to '/sign_in'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You have signed out."
+    redirect_to '/sign_in'
+  end
+
+end
